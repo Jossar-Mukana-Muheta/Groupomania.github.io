7816e28e3035662e0e92f9cd0657b1a291f22d46
Jossar Mukana Muheta <muhetajossar@gmail.com>
2020-07-06 22:50:40 +0200

    page connexion v1

---
+++ src/components/AccueilLogo.vue
@@ -0,0 +1,24 @@
+<template>
+
+    <div>
+        <img src="../assets/logo.png" alt="logo" >
+        <h1>Bienvenue chez Groupamania</h1>
+    </div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+img{
+    height: 300px;
+}
+
+h1{
+    text-align: center;
+}
+</style>
---
+++ src/components/Boutton.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <button type="submit">{{text}}</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Button",
+    props: ["text"]
+}
+</script>
+
+<style lang="scss">
+
+button{
+    background-color: orangered;
+    border-radius: 17px;
+    height: 36px;
+    width: 110px;
+    font-weight: 800;
+}
+
+</style>
